b13d01f217fda009585988a79d5945dfe911340d
Julien HENRY <julien.henry@sonarsource.com>
2015-11-13 11:28:59 +0100

    Enable PR analysis

--- travis.sh
+++ travis.sh
@@ -12,7 +12,35 @@ installTravisTools
 
 build_snapshot SonarSource/sonar-runner
 
-./gradlew buildPlugin check
+if [ -n "${PR_ANALYSIS:-}" ] && [ "${PR_ANALYSIS}" == true ]
+then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    # For security reasons environment variables are not available on the pull requests
+    # coming from outside repositories
+    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
+    if [ -n "$SONAR_GITHUB_OAUTH" ]; then
+
+      # PR analysis
+      # Accessing Dory with SSL requires Java 8
+      source $HOME/.jdk_switcher_rc
+      jdk_switcher use oraclejdk8    
+      ./gradlew sonarqube \
+        -Dsonar.analysis.mode=issues \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
+        -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_LOGIN \
+        -Dsonar.password=$SONAR_PASSWD
+    fi
+  fi
+else
+  # Regular CI
+  ./gradlew buildPlugin check
+fi
+
 
 
 
