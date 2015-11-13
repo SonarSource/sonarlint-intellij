b1e0ad3f9d7ee6d6a1c39305d4151cf90b513333
Julien HENRY <julien.henry@sonarsource.com>
2015-11-13 11:24:21 +0100

    SLI-7 Display precise issue location

--- src/main/java/org/sonarlint/intellij/inspection/SonarLintGlobalInspection.java
+++ src/main/java/org/sonarlint/intellij/inspection/SonarLintGlobalInspection.java
@@ -2,17 +2,17 @@
  * SonarLint for IntelliJ IDEA
  * Copyright (C) 2015 SonarSource
  * sonarlint@sonarsource.com
- *
+ * <p/>
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * <p/>
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
@@ -36,8 +36,11 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
@@ -52,7 +55,7 @@ public class SonarLintGlobalInspection extends GlobalInspectionTool {
   @Nullable
   private static String realFilePath(PsiFile psiFile) {
     if (!existsOnFilesystem(psiFile)
-      || documentIsModifiedAndUnsaved(psiFile.getVirtualFile())) {
+        || documentIsModifiedAndUnsaved(psiFile.getVirtualFile())) {
       return null;
     } else {
       return pathOf(psiFile);
@@ -87,7 +90,7 @@ public class SonarLintGlobalInspection extends GlobalInspectionTool {
 
   @Override
   public void runInspection(@NotNull final AnalysisScope scope, @NotNull final InspectionManager manager, @NotNull final GlobalInspectionContext globalContext,
-    @NotNull final ProblemDescriptionsProcessor problemDescriptionsProcessor) {
+                            @NotNull final ProblemDescriptionsProcessor problemDescriptionsProcessor) {
     final Project project = globalContext.getProject();
     final Multimap<Module, String> filesToAnalyzeByModule = findFilesToAnalyze(scope);
     for (Module m : filesToAnalyzeByModule.keySet()) {
@@ -141,20 +144,66 @@ public class SonarLintGlobalInspection extends GlobalInspectionTool {
   }
 
   ProblemDescriptor getProblemDescriptor(Issue issue, PsiFile psiFile, @NotNull InspectionManager manager, boolean isLocal) {
-    if (issue.getLine() != null) {
-      return manager.createProblemDescriptor(psiFile,
-        InspectionUtils.getLineRange(psiFile, issue.getLine()),
+    Integer startLine = issue.getStartLine();
+    if (startLine == null) {
+      // No line, global issue
+      return createProblemDescriptorFileLevel(issue, psiFile, manager, isLocal);
+    }
+    Integer startLineOffset = issue.getStartLineOffset();
+    if (startLineOffset == null) {
+      // No offset, either old SQ version or language plugin doesn't provide data. Fallback to text range
+      return createProblemDescriptorPreciseTextRange(issue, psiFile, manager, isLocal, InspectionUtils.getLineRange(psiFile, startLine));
+    }
+    final Document document = PsiDocumentManager.getInstance(psiFile.getProject()).getDocument(psiFile);
+    if (document == null) {
+      // I'm not sure when this can occurs. Create a global issue
+      return createProblemDescriptorFileLevel(issue, psiFile, manager, isLocal);
+    }
+    return tryCreateProblemDescriptorPrecisePsiElement(issue, psiFile, manager, isLocal, startLine, startLineOffset, document);
+  }
+
+  @NotNull
+  private ProblemDescriptor tryCreateProblemDescriptorPrecisePsiElement(Issue issue, PsiFile psiFile, @NotNull InspectionManager manager, boolean isLocal, Integer startLine, Integer startLineOffset, Document document) {
+    Integer endLine = issue.getEndLine();
+    Integer endLineOffset = issue.getEndLineOffset();
+    int ijStartLine = startLine - 1;
+    int ijEndLine = endLine - 1;
+    PsiElement startElement;
+    PsiElement endElement;
+    int lineStartOffset = document.getLineStartOffset(ijStartLine);
+    final int startOffset = lineStartOffset + startLineOffset;
+    final int endOffset = (ijEndLine == ijStartLine) ? (lineStartOffset + endLineOffset) : (document.getLineStartOffset(ijEndLine) + endLineOffset);
+    startElement = psiFile.getViewProvider().findElementAt(startOffset);
+    if (startElement == null || startElement.getTextRange().getStartOffset() != startOffset) {
+      // Start element is not exactly the good one, fallback on text range
+      return createProblemDescriptorPreciseTextRange(issue, psiFile, manager, isLocal, new TextRange(startOffset, endOffset));
+    }
+    endElement = psiFile.getViewProvider().findElementAt(endOffset - 1);
+    if (endElement == null || endElement.getTextRange().getEndOffset() != endOffset) {
+      // End element is not exactly the good one, fallback on text range
+      return createProblemDescriptorPreciseTextRange(issue, psiFile, manager, isLocal, new TextRange(startOffset, endOffset));
+    }
+    return manager.createProblemDescriptor(startElement, endElement,
         InspectionUtils.getProblemMessage(issue, isLocal),
         problemHighlightType(issue, isLocal),
         false);
-    } else {
-      return manager.createProblemDescriptor(psiFile,
+  }
+
+  private ProblemDescriptor createProblemDescriptorPreciseTextRange(Issue issue, PsiFile psiFile, @NotNull InspectionManager manager, boolean isLocal, TextRange rangeInElement) {
+    return manager.createProblemDescriptor(psiFile,
+        rangeInElement,
+        InspectionUtils.getProblemMessage(issue, isLocal),
+        problemHighlightType(issue, isLocal),
+        false);
+  }
+
+  private ProblemDescriptor createProblemDescriptorFileLevel(Issue issue, PsiFile psiFile, @NotNull InspectionManager manager, boolean isLocal) {
+    return manager.createProblemDescriptor(psiFile,
         InspectionUtils.getProblemMessage(issue, isLocal),
         new LocalQuickFix[0],
         problemHighlightType(issue, isLocal),
         false,
         false);
-    }
   }
 
   ProblemHighlightType problemHighlightType(Issue issue, boolean isLocal) {
