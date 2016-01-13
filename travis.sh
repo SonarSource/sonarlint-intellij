7b5ef2e38cb461c83d9fb83136cfa6bcaba97624
Julien HENRY <julien.henry@sonarsource.com>
2016-01-13 21:39:19 +0100

    Enable Dory analysis

--- travis.sh
+++ travis.sh
@@ -8,40 +8,60 @@ function installTravisTools {
   source ~/.local/bin/install
 }
 
-installTravisTools
+function strongEcho {
+  echo ""
+  echo "================ $1 ================="
+}
 
-build_snapshot SonarSource/sonar-runner
+unset DISPLAY
 
-if [ -n "${PR_ANALYSIS:-}" ] && [ "${PR_ANALYSIS}" == true ]
-then
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-  then
-    # For security reasons environment variables are not available on the pull requests
-    # coming from outside repositories
-    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
-    if [ -n "$SONAR_GITHUB_OAUTH" ]; then
+case "$TARGET" in
 
-      # PR analysis
-      # Accessing Dory with SSL requires Java 8
-      source $HOME/.jdk_switcher_rc
-      jdk_switcher use oraclejdk8    
-      ./gradlew sonarqube \
-        -Dsonar.analysis.mode=issues \
+CI)
+  if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    strongEcho 'Build and analyze commit in master'
+    # this commit is master must be built and analyzed (with upload of report)
+    ./gradlew buildPlugin check sonarqube \
+        -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit --stacktrace \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_TOKEN
+
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "$GITHUB_TOKEN" ]; then
+    # For security reasons environment variables are not available on the pull requests coming from outside repositories
+    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests                                                                         
+    # That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.                                                                
+                                                                                                                                                                             
+    strongEcho 'Build and analyze pull request'                                                                                                                              
+    # this pull request must be built and analyzed (without upload of report)                                                                                                
+    ./gradlew buildPlugin check sonarqube \
+        -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit --stacktrace \
+        -Dsonar.analysis.mode=issues \                                                                                                                                       
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
-        -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
         -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_LOGIN \
-        -Dsonar.password=$SONAR_PASSWD
-    fi
-  fi
-else
-  # Regular CI
-  unset DISPLAY
-  ./gradlew buildPlugin check -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit -PijVersion=$IJ_VERSION --debug
-fi
+        -Dsonar.login=$SONAR_TOKEN
 
+  else
+    strongEcho 'Build, no analysis'
+    # Build branch, without any analysis
+
+    ./gradlew buildPlugin check -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit --stacktrace
+
+  fi
+  ;;
+
+IT)
+  ./gradlew buildPlugin check -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit -PijVersion=$IDEA_VERSION --stacktrace
+
+  ;;
+
+*)
+  echo "Unexpected TARGET value: $TARGET"
+  exit 1
+  ;;
+
+esac
 
 
 
