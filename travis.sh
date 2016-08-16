a99e4eeae070e069d72e53e7664cd4e4a076e9b6
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-08-16 12:57:51 +0200

    publish to artifactory in master

--- travis.sh
+++ travis.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# Requires the environment variables:
+# - SONAR_HOST_URL: URL of SonarQube server
+# - SONAR_TOKEN: access token to send analysis reports to $SONAR_HOST_URL
+# - GITHUB_TOKEN: access token to send analysis of pull requests to GibHub
+# - ARTIFACTORY_URL: URL to Artifactory repository
+# - ARTIFACTORY_DEPLOY_REPO: name of deployment repository
+# - ARTIFACTORY_DEPLOY_USERNAME: login to deploy to $ARTIFACTORY_DEPLOY_REPO
+# - ARTIFACTORY_DEPLOY_PASSWORD: password to deploy to $ARTIFACTORY_DEPLOY_REPO
 
 set -euo pipefail
 
@@ -8,11 +17,19 @@ function installTravisTools {
   source ~/.local/bin/install
 }
 
-installTravisTools
-
 function strongEcho {
   echo ""
   echo "================ $1 ================="
+}
+
+function prepareBuildVersion {
+    # Analyze with SNAPSHOT version as long as SQ does not correctly handle purge of release data
+    CURRENT_VERSION=`cat gradle.properties | grep version | awk -F= '{print $2}'`
+    # Deply the release version related to this build instead of snapshot
+    sed -i.bak "s/-SNAPSHOT/-build$TRAVIS_BUILD_NUMBER/g" gradle.properties
+    # set the build name with travis build number
+    echo buildInfo.build.name=sonarlint-intellij >> gradle.properties 
+    echo buildInfo.build.number=$TRAVIS_BUILD_NUMBER >> gradle.properties
 }
 
 unset DISPLAY
@@ -21,41 +38,24 @@ case "$TARGET" in
 
 CI)
   if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    strongEcho 'Build and analyze commit in master'
+    strongEcho 'Build and analyze commit in master and publish in artifactory'
     # this commit is master must be built and analyzed (with upload of report)
     git fetch --unshallow || true
-    ./gradlew buildPlugin check sonarqube \
+    prepareBuildVersion
+    ./gradlew buildPlugin check sonarqube artifactory \
         -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit --stacktrace --info \
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_TOKEN
 
   elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
     strongEcho 'Build and analyze pull request'                                                                                                                              
-    # this pull request must be built and analyzed (without upload of report)                                                                                                
+    # this pull request must be built and analyzed (without upload of report)
     ./gradlew buildPlugin check sonarqube \
         -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit --stacktrace \
         -Dsonar.analysis.mode=issues \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$GITHUB_TOKEN \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN
-
-  elif [ "$TRAVIS_BRANCH" == "feature/cix" ]; then
-    strongEcho 'feature/cix: build analyse deploy on repox'
-    
-    # Analyze with SNAPSHOT version as long as SQ does not correctly handle
-    # purge of release data
-    CURRENT_VERSION=`cat gradle.properties | grep version | awk -F= '{print $2}'`
-    # Do not deploy a SNAPSHOT version but the release version related to this build
-    sed -i.bak "s/-SNAPSHOT/-build$TRAVIS_BUILD_NUMBER/g" gradle.properties
-    # set the build name with travis build number
-    echo buildInfo.build.name=sonarlint-intellij >> gradle.properties 
-    echo buildInfo.build.number=$TRAVIS_BUILD_NUMBER >> gradle.properties 
-
-
-    ./gradlew build check sonarqube artifactory \
-        -Dsonar.projectVersion=$CURRENT_VERSION \
         -Dsonar.host.url=$SONAR_HOST_URL \
         -Dsonar.login=$SONAR_TOKEN
 
