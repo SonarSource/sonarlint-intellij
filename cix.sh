09208690d6e97b2d0c6d11e9d0350563dddc799f
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-11-15 16:50:03 +0100

    SLI-123 Add ITs standalone mode

--- cix.sh
+++ cix.sh
@@ -4,3 +4,7 @@ echo "Running with IDEA_VERSION=$IDEA_VERSION"
     
 ./gradlew buildPlugin check -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit -PijVersion=$IDEA_VERSION --stacktrace
 
+echo "\n\n#### Running ITs ####\n\n"
+
+./gradlew -p its check --stacktrace
+
---
+++ its/src/test/java/org/sonarlint/intellij/its/TestClientInputFile.java
@@ -0,0 +1,70 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015 SonarSource
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.its;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.sonarsource.sonarlint.core.client.api.common.analysis.ClientInputFile;
+
+public class TestClientInputFile implements ClientInputFile {
+  private Path path;
+  private boolean isTest;
+  private Charset encoding;
+
+  public TestClientInputFile(final Path path, final boolean isTest, final Charset encoding) {
+    this.path = path;
+    this.isTest = isTest;
+    this.encoding = encoding;
+  }
+
+  @Override
+  public String getPath() {
+    return path.toString();
+  }
+
+  @Override
+  public boolean isTest() {
+    return isTest;
+  }
+
+  @Override
+  public Charset getCharset() {
+    return encoding;
+  }
+
+  @Override
+  public <G> G getClientObject() {
+    return null;
+  }
+
+  @Override
+  public InputStream inputStream() throws IOException {
+    return Files.newInputStream(path);
+  }
+
+  @Override
+  public String contents() throws IOException {
+    return new String(Files.readAllBytes(path), encoding);
+  }
+}
