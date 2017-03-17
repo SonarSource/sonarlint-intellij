5219cc275552bc864e68c903a5efc0e427191e9d
Duarte Meneses <duarte.meneses@sonarsource.com>
2017-03-17 10:55:58 +0100

    Fix zip fs

--- src/main/java/org/sonarlint/intellij/core/SonarLintEngineFactory.java
+++ src/main/java/org/sonarlint/intellij/core/SonarLintEngineFactory.java
@@ -25,11 +25,15 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import org.sonarlint.intellij.util.GlobalLogOutput;
 import org.sonarsource.sonarlint.core.ConnectedSonarLintEngineImpl;
@@ -93,10 +97,14 @@ public class SonarLintEngineFactory extends ApplicationComponent.Adapter {
     }
 
     List<URL> pluginsUrls = new ArrayList<>();
-    try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(pluginsDir.toURI()), "*.jar")) {
-      for (Path path : directoryStream) {
-        globalLogOutput.log("Found plugin: " + path.getFileName().toString(), LogOutput.Level.DEBUG);
-        pluginsUrls.add(path.toUri().toURL());
+    Map<String, String> env = new HashMap<>();
+    env.put("create", "true");
+    try (FileSystem zipfs = FileSystems.newFileSystem(pluginsDir.toURI(), env)) {
+      try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(pluginsDir.toURI()), "*.jar")) {
+        for (Path path : directoryStream) {
+          globalLogOutput.log("Found plugin: " + path.getFileName().toString(), LogOutput.Level.DEBUG);
+          pluginsUrls.add(path.toUri().toURL());
+        }
       }
     }
     return pluginsUrls.toArray(new URL[pluginsUrls.size()]);
