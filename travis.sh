2a892b62243a14e4a2cd923a9bb73c0e83a5b608
Simon Brandhof <simon.brandhof@sonarsource.com>
2019-03-11 10:25:32 +0100

    Fix build on Travis

--- travis.sh
+++ travis.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v40 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v55 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
