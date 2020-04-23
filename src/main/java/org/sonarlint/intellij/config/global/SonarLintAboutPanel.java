5a18ee4aed100cb98b22440906f36d077157661c
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2020-04-23 19:53:23 +0200

    SLI-385 Update telemetry snippet

--- src/main/java/org/sonarlint/intellij/config/global/SonarLintAboutPanel.java
+++ src/main/java/org/sonarlint/intellij/config/global/SonarLintAboutPanel.java
@@ -95,16 +95,20 @@ public class SonarLintAboutPanel implements ConfigurationPanel<SonarLintTelemetr
     link.addHyperlinkListener(new HyperlinkAdapter() {
       @Override
       protected void hyperlinkActivated(HyperlinkEvent e) {
-        final JLabel label = new JLabel("<html><pre>{\n"
+        final JLabel label = new JLabel("{\n"
           + "    \"days_since_installation\": 120,\n"
           + "    \"days_of_use\": 40,\n"
           + "    \"sonarlint_version\": \"2.9\",\n"
           + "    \"sonarlint_product\": \"SonarLint IntelliJ\",\n"
-          + "    \"connected_mode_used\": true\n"
+          + "    \"ide_version\": \"IntelliJ IDEA 2020.1\",\n"
+          + "    \"os\": \"Linux\",\n"
+          + "    \"jre\": \"11.0.6\",\n"
+          + "    \"connected_mode_used\": true,\n"
+          + "    \"connected_mode_sonarcloud\": false,\n"
           + "    \"system_time\":\"2018-06-27T16:31:49.173+01:00\",\n"
-          + "    \"install_time\":\"2018-02-27T16:30:49.124+01:00\"\n"
+          + "    \"install_time\":\"2018-02-27T16:30:49.124+01:00\",\n"
           + "    \"analyses\":[{\"language\":\"java\",\"rate_per_duration\":{\"0-300\":100,\"300-500\":0,\"500-1000\":0,\"1000-2000\":0,\"2000-4000\":0,\"4000+\":0}}]\n"
-          + "}</pre><html>");
+          + "}");
         label.setBorder(HintUtil.createHintBorder());
         label.setBackground(HintUtil.getInformationColor());
         label.setOpaque(true);
