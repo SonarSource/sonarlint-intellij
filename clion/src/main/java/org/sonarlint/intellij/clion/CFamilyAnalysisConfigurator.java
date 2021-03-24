ae6d6754a7d66bbcc5ef2f3859ce1988364d4472
Massimo Paladin <massimo.paladin@gmail.com>
2021-03-24 10:54:46 +0100

    SLI-509 Generate build-wrapper-content

--- clion/src/main/java/org/sonarlint/intellij/clion/CFamilyAnalysisConfigurator.java
+++ clion/src/main/java/org/sonarlint/intellij/clion/CFamilyAnalysisConfigurator.java
@@ -2,26 +2,25 @@ package org.sonarlint.intellij.clion;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.cidr.lang.workspace.OCResolveConfiguration;
-import com.jetbrains.cidr.lang.workspace.OCWorkspaceRunConfigurationListener;
+import org.sonarlint.intellij.common.analysis.AnalysisConfigurator;
+
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import org.sonarlint.intellij.common.analysis.AnalysisConfigurator;
+import java.util.Objects;
 
 public class CFamilyAnalysisConfigurator implements AnalysisConfigurator {
 
   @Override
   public Map<String, String> configure(Module module, Collection<VirtualFile> filesToAnalyze) {
-    OCResolveConfiguration configuration = OCWorkspaceRunConfigurationListener.getSelectedResolveConfiguration(module.getProject());
-    if (configuration != null) {
-      Map<String, String> result = new HashMap<>();
-      String buildWrapperJson = CLionConfiguration.BuildWrapperJsonFactory.create(module.getProject(), configuration, filesToAnalyze);
-
-      result.put("sonar.cfamily.build-wrapper-content", buildWrapperJson);
-      return result;
-    }
-    return Collections.emptyMap();
+    AnalyzerConfiguration analyzerConfiguration = new AnalyzerConfiguration(module.getProject());
+    BuildWrapperJsonGenerator buildWrapperJsonGenerator = new BuildWrapperJsonGenerator();
+    filesToAnalyze.stream()
+      .map(analyzerConfiguration::getCompilerSettings)
+      .filter(Objects::nonNull)
+      .filter(request -> "clang".equals(request.compiler))
+      .forEach(buildWrapperJsonGenerator::addRequest);
+    return Collections.singletonMap("sonar.cfamily.build-wrapper-content", buildWrapperJsonGenerator.build());
   }
+
 }
