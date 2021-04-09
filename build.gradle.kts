8035e6942ec3d27a44e468639c155e5f36cd8525
Julien HENRY <julien.henry@sonarsource.com>
2021-04-09 11:11:09 +0200

    SLI-520 Update embedded PHP analyzer to 3.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,7 +128,7 @@ dependencies {
     testImplementation("org.eclipse.jetty:jetty-proxy:$jettyVersion")
     "sqplugins"("org.sonarsource.java:sonar-java-plugin:6.14.0.25463@jar")
     "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:7.3.0.15071@jar")
-    "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.15.0.7197@jar")
+    "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.16.0.7320@jar")
     "sqplugins"("org.sonarsource.python:sonar-python-plugin:3.2.0.7856@jar")
     "sqplugins"("org.sonarsource.slang:sonar-kotlin-plugin:1.8.2.1946@jar")
     "sqplugins"("org.sonarsource.slang:sonar-ruby-plugin:1.8.2.1946@jar")
