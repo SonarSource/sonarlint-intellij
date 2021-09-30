4848ce7b3536a3e2d5ef760674fab6fdfdd6a519
Damien Urruty <damien.urruty@sonarsource.com>
2021-09-30 17:42:14 +0200

    SLI-171 Add an IT to verify binding process

---
+++ its/src/test/kotlin/org/sonarlint/intellij/its/fixtures/JBTableFixture.kt
@@ -0,0 +1,35 @@
+/*
+ * SonarLint for IntelliJ IDEA ITs
+ * Copyright (C) 2015-2021 SonarSource
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.its.fixtures
+
+import com.intellij.remoterobot.RemoteRobot
+import com.intellij.remoterobot.data.RemoteComponent
+import com.intellij.remoterobot.fixtures.CommonContainerFixture
+import com.intellij.remoterobot.fixtures.FixtureName
+
+@FixtureName("JBTable")
+class JBTableFixture(
+    remoteRobot: RemoteRobot,
+    remoteComponent: RemoteComponent
+) : CommonContainerFixture(remoteRobot, remoteComponent) {
+    fun selectItemContaining(content: String) {
+        runJs("JTableFixture(robot, component).cell(\".*$content.*\").click();", true)
+    }
+}
