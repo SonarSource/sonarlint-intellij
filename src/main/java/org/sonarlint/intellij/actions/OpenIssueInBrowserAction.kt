f474b549ae2a4a80204e4b8100625dc29f09c07b
Damien Urruty <damien.urruty@sonarsource.com>
2021-10-12 15:06:29 +0200

    SLI-171 Use module binding when configured to open an issue in the browser

--- src/main/java/org/sonarlint/intellij/actions/OpenIssueInBrowserAction.kt
+++ src/main/java/org/sonarlint/intellij/actions/OpenIssueInBrowserAction.kt
@@ -22,11 +22,12 @@ package org.sonarlint.intellij.actions
 import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataKey
+import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
 import org.sonarlint.intellij.analysis.AnalysisStatus
 import org.sonarlint.intellij.common.util.SonarLintUtils.getService
-import org.sonarlint.intellij.config.Settings.getSettingsFor
 import org.sonarlint.intellij.config.global.ServerConnection
+import org.sonarlint.intellij.core.ModuleBindingManager
 import org.sonarlint.intellij.core.ProjectBindingManager
 import org.sonarlint.intellij.issue.vulnerabilities.LocalTaintVulnerability
 import org.sonarlint.intellij.telemetry.SonarLintTelemetry
@@ -53,8 +54,11 @@ class OpenIssueInBrowserAction : AbstractSonarAction(
     val project = e.project ?: return
     val issue = e.getData(TAINT_VULNERABILITY_DATA_KEY)
     val key = issue?.key() ?: return
+    val localFile = issue.file() ?: return
+    val localFileModule = ModuleUtil.findModuleForFile(localFile, project) ?: return
     val serverUrl = serverConnection(project)?.hostUrl ?: return
-    BrowserUtil.browse(buildLink(serverUrl, getSettingsFor(project).projectKey!!, key))
+    val projectKey = getService(localFileModule, ModuleBindingManager::class.java).resolveProjectKey() ?: return
+    BrowserUtil.browse(buildLink(serverUrl, projectKey, key))
     getService(SonarLintTelemetry::class.java).taintVulnerabilitiesInvestigatedRemotely()
   }
 
