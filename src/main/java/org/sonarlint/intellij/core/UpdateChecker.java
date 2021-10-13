984fbf0d2a2d459766ca392d2d02698d583227aa
Knize <62898885+kirill-knize-sonarsource@users.noreply.github.com>
2021-10-13 11:53:16 +0200

    SLI-171 consider module overrides in update checker (#455)
    * SLI-171 consider module overrides in update checker

--- src/main/java/org/sonarlint/intellij/core/UpdateChecker.java
+++ src/main/java/org/sonarlint/intellij/core/UpdateChecker.java
@@ -28,11 +28,11 @@ import com.intellij.openapi.project.Project;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.jetbrains.annotations.NotNull;
-import org.sonarlint.intellij.common.util.SonarLintUtils;
 import org.sonarlint.intellij.config.global.ServerConnection;
 import org.sonarlint.intellij.config.project.SonarLintProjectSettings;
 import org.sonarlint.intellij.notifications.SonarLintProjectNotifications;
@@ -42,10 +42,13 @@ import org.sonarsource.sonarlint.core.client.api.common.LogOutput;
 import org.sonarsource.sonarlint.core.client.api.connected.ConnectedSonarLintEngine;
 import org.sonarsource.sonarlint.core.client.api.connected.StorageUpdateCheckResult;
 
+import static org.sonarlint.intellij.common.util.SonarLintUtils.getService;
 import static org.sonarlint.intellij.config.Settings.getSettingsFor;
 
 public class UpdateChecker implements Disposable {
 
+  private static final double START_OF_PROJECTS_UPDATE = 0.0;
+  private static final double END_OF_PROJECTS_UPDATE = 0.8;
   private final Project myProject;
   private ScheduledFuture<?> scheduledTask;
 
@@ -69,10 +72,10 @@ public class UpdateChecker implements Disposable {
 
   void checkForUpdate(@NotNull ProgressIndicator progressIndicator) {
     ProjectBindingManager projectBindingManager;
-    GlobalLogOutput log = SonarLintUtils.getService(GlobalLogOutput.class);
+    GlobalLogOutput log = getService(GlobalLogOutput.class);
     ConnectedSonarLintEngine engine;
     try {
-      projectBindingManager = SonarLintUtils.getService(myProject, ProjectBindingManager.class);
+      projectBindingManager = getService(myProject, ProjectBindingManager.class);
       engine = projectBindingManager.getConnectedEngine();
     } catch (Exception e) {
       // happens if project is not bound, binding is invalid, storages are not updated, ...
@@ -85,16 +88,14 @@ public class UpdateChecker implements Disposable {
       ServerConnection serverConnection = projectBindingManager.getServerConnection();
       log.log("Check for updates from server '" + serverConnection.getName() + "'...", LogOutput.Level.INFO);
       progressIndicator.setIndeterminate(false);
-      progressIndicator.setFraction(0.0);
+      progressIndicator.setFraction(START_OF_PROJECTS_UPDATE);
       boolean hasGlobalUpdates = checkForGlobalUpdates(changelog, engine, serverConnection, progressIndicator);
       SonarLintProjectSettings projectSettings = getSettingsFor(myProject);
-      log.log("Check for updates from server '" + serverConnection.getName() +
-        "' for project '" + projectSettings.getProjectKey() + "'...", LogOutput.Level.INFO);
-      progressIndicator.setFraction(0.8);
       checkForProjectUpdates(changelog, engine, serverConnection, progressIndicator);
+      progressIndicator.setFraction(END_OF_PROJECTS_UPDATE);
       if (!changelog.isEmpty()) {
         changelog.forEach(line -> log.log("  - " + line, LogOutput.Level.INFO));
-        SonarLintProjectNotifications notifications = SonarLintUtils.getService(myProject, SonarLintProjectNotifications.class);
+        SonarLintProjectNotifications notifications = getService(myProject, SonarLintProjectNotifications.class);
         notifications.notifyServerHasUpdates(projectSettings.getConnectionName(), engine, serverConnection, !hasGlobalUpdates);
       }
     } catch (Exception e) {
@@ -103,9 +104,22 @@ public class UpdateChecker implements Disposable {
   }
 
   private void checkForProjectUpdates(List<String> changelog, ConnectedSonarLintEngine engine, ServerConnection serverConnection, ProgressIndicator indicator) {
+    Set<String> projectKeysToUpdate = getService(myProject, ProjectBindingManager.class).getUniqueProjectKeys();
+    double indicatorValue = START_OF_PROJECTS_UPDATE;
+    double divider = projectKeysToUpdate.isEmpty() ? 1 : projectKeysToUpdate.size();
+    double stepSize = (END_OF_PROJECTS_UPDATE - START_OF_PROJECTS_UPDATE) / divider;
+    GlobalLogOutput log = getService(GlobalLogOutput.class);
+    for (String projectKey : projectKeysToUpdate) {
+      log.log("Check for updates from server '" + serverConnection.getName() + "' for project '" + projectKey + "'...", LogOutput.Level.INFO);
+      checkForProjectKey(changelog, engine, serverConnection, indicator, projectKey);
+      indicatorValue += stepSize;
+      indicator.setFraction(indicatorValue);
+    }
+  }
+
+  private void checkForProjectKey(List<String> changelog, ConnectedSonarLintEngine engine, ServerConnection serverConnection, ProgressIndicator indicator, String projectKey) {
     StorageUpdateCheckResult projectUpdateCheckResult = engine.checkIfProjectStorageNeedUpdate(serverConnection.getEndpointParams(), serverConnection.getHttpClient(),
-      getSettingsFor(myProject).getProjectKey(),
-      new TaskProgressMonitor(indicator, myProject));
+      projectKey, new TaskProgressMonitor(indicator, myProject));
     if (projectUpdateCheckResult.needUpdate()) {
       changelog.addAll(projectUpdateCheckResult.changelog());
     }
