f18af8d0144696f59a052eb13b92107ae7ec00d9
Damien Urruty <damien.urruty@sonarsource.com>
2022-03-15 10:47:47 +0100

    Fix an object comparison

--- src/main/java/org/sonarlint/intellij/core/ProjectBindingManager.java
+++ src/main/java/org/sonarlint/intellij/core/ProjectBindingManager.java
@@ -28,6 +28,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -177,7 +178,7 @@ public class ProjectBindingManager {
 
     SonarLintProjectNotifications.get(myProject).reset();
     var newBinding = getBinding();
-    if (previousBinding != newBinding) {
+    if (!Objects.equals(previousBinding, newBinding)) {
       myProject.getMessageBus().syncPublisher(ProjectBindingListenerKt.getPROJECT_BINDING_TOPIC()).bindingChanged(previousBinding, newBinding);
     }
     var task = new BindingStorageUpdateTask(getConnectedEngineSkipChecks(), connection, false, true, myProject);
