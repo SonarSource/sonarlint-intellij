e8c9dd389a27996d94f0527e9ab69df8bd5bb80f
Damien Urruty <damien.urruty@sonarsource.com>
2022-05-25 17:22:15 +0200

    SLI-712 Update embedded JavaScript analyzer to 9.2.0.17876

--- build.gradle.kts
+++ build.gradle.kts
@@ -199,7 +199,7 @@ dependencies {
     testImplementation("org.eclipse.jetty:jetty-servlet:$jettyVersion")
     testImplementation("org.eclipse.jetty:jetty-proxy:$jettyVersion")
     "sqplugins"("org.sonarsource.java:sonar-java-plugin:7.11.0.29148")
-    "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:9.1.0.17747")
+    "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:9.2.0.17876")
     "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.23.0.8726")
     "sqplugins"("org.sonarsource.python:sonar-python-plugin:3.12.0.9583")
     "sqplugins"("org.sonarsource.kotlin:sonar-kotlin-plugin:2.9.0.1147")
