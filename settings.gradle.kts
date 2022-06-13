98698077a142a2f134efb804ee7aef757e3e00bb
Julien HENRY <julien.henry@sonarsource.com>
2022-06-13 15:01:32 +0200

    Use SNAPSHOT version of the Intellij Gradle Plugin
    To fix issue of tests packaged into final distribution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
 rootProject.name = "sonarlint-intellij"
 include("its", "clion", "common", "git", "rider")
 
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
