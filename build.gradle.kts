630639ef0cd8a3f7605ace7ad69c2b2e73456a7e
Damien Urruty <damien.urruty@sonarsource.com>
2022-09-20 16:49:59 +0200

    SLI-768 Update embedded Python analyzer to 3.17.0.10029

--- build.gradle.kts
+++ build.gradle.kts
@@ -185,7 +185,7 @@ dependencies {
     "sqplugins"("org.sonarsource.java:sonar-java-plugin:7.13.0.29990")
     "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:9.4.0.18205")
     "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.23.1.8766")
-    "sqplugins"("org.sonarsource.python:sonar-python-plugin:3.15.1.9817")
+    "sqplugins"("org.sonarsource.python:sonar-python-plugin:3.17.0.10029")
     "sqplugins"("org.sonarsource.kotlin:sonar-kotlin-plugin:2.10.0.1456")
     "sqplugins"("org.sonarsource.slang:sonar-ruby-plugin:1.10.0.3710")
     "sqplugins"("org.sonarsource.html:sonar-html-plugin:3.6.0.3106")
