086a086605a20aa43c2c18af31169199a4847911
Damien Urruty <damien.urruty@sonarsource.com>
2023-01-12 13:35:09 +0100

    SLI-845 Update embedded Java analyzer to 7.16.0.30901

--- build.gradle.kts
+++ build.gradle.kts
@@ -184,7 +184,7 @@ dependencies {
     testImplementation("org.eclipse.jetty:jetty-server:$jettyVersion")
     testImplementation("org.eclipse.jetty:jetty-servlet:$jettyVersion")
     testImplementation("org.eclipse.jetty:jetty-proxy:$jettyVersion")
-    "sqplugins"("org.sonarsource.java:sonar-java-plugin:7.15.0.30507")
+    "sqplugins"("org.sonarsource.java:sonar-java-plugin:7.16.0.30901")
     "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:9.12.1.20358")
     "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.27.0.9339")
     "sqplugins"("org.sonarsource.python:sonar-python-plugin:3.21.0.10628")
