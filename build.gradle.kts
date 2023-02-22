3d9ded04956e65af0e669e26b11dec4d5d40e91a
Damien Urruty <damien.urruty@sonarsource.com>
2023-02-22 18:20:24 +0100

    SLI-862 Update embedded C# analyzer to 8.53.0.62665

--- build.gradle.kts
+++ build.gradle.kts
@@ -212,7 +212,7 @@ dependencies {
     "sqplugins"("org.sonarsource.slang:sonar-ruby-plugin:1.11.0.3905")
     "sqplugins"("org.sonarsource.html:sonar-html-plugin:3.7.1.3306")
     "sqplugins"("org.sonarsource.xml:sonar-xml-plugin:2.7.0.3820")
-    "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.7.0.62470")
+    "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.7.0.62958")
     "sqplugins"("org.sonarsource.text:sonar-text-plugin:2.0.1.611")
     if (artifactoryUsername.isNotEmpty() && artifactoryPassword.isNotEmpty()) {
         "sqplugins"("com.sonarsource.cpp:sonar-cfamily-plugin:6.42.0.61084")
