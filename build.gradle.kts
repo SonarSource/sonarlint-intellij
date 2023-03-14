808ebfc57abde8f703e5aea7c0efde4004ff897b
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-03-14 10:35:23 +0100

    SLi-890 Update embedded PHP analyzer to 3.28.0.9490
    Revert "Analyzers update for 8.1"
    
    This reverts commit 35af81382f5b6a4a3c9f1ac1fb7add3700a37caa.
    
    SLI-890 Update embedded PHP analyzer to 3.28.0.9490

--- build.gradle.kts
+++ build.gradle.kts
@@ -205,7 +205,7 @@ dependencies {
     testImplementation("org.eclipse.jetty:jetty-proxy:$jettyVersion")
     "sqplugins"("org.sonarsource.java:sonar-java-plugin:7.17.0.31219")
     "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:10.0.1.20755")
-    "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.27.0.9339")
+    "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.28.0.9490")
     "sqplugins"("org.sonarsource.python:sonar-python-plugin:4.0.0.11155")
     "sqplugins"("org.sonarsource.kotlin:sonar-kotlin-plugin:2.12.0.1956")
     "sqplugins"("org.sonarsource.slang:sonar-ruby-plugin:1.11.0.3905")
