8eadb126338268a1047f6a66b01d03d396fa614d
Damien Urruty <damien.urruty@sonarsource.com>
2023-03-16 11:45:20 +0100

    SLI-765 Silence quick fixes not matching IntelliJ experience

--- src/test/testData/StandaloneModeMediumTests/src/quick_fixes/overlapping_quick_fixes.expected.java
+++ src/test/testData/StandaloneModeMediumTests/src/quick_fixes/overlapping_quick_fixes.expected.java
@@ -1,3 +1,13 @@
+import java.io.File;
+import java.util.Arrays;
+
 public class QuickFixableFile {
-  <caret>
+  void m() {
+    var file = new File("");
+    var array = new int[5];
+    if (file != null && (file.isFile() || Arrays.toString(array).equals(""))) {
+        /* ... */
+      
+    }
+  }
 }
--- src/test/testData/StandaloneModeMediumTests/src/quick_fixes/overlapping_quick_fixes.input.java
+++ src/test/testData/StandaloneModeMediumTests/src/quick_fixes/overlapping_quick_fixes.input.java
@@ -1,3 +1,14 @@
+import java.io.File;
+import java.util.Arrays;
+
 public class QuickFixableFile {
-  private List<String> strings = new ArrayList<Stri<caret>ng>();
+  void m() {
+    var file = new File("");
+    var array = new int[5];
+    if (file != null) {
+      if (file.isFile() || array.to<caret>String().equals("")) {
+        /* ... */
+      }
+    }
+  }
 }
