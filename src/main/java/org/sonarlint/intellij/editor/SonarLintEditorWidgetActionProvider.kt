ccb5d02996fdf44d6605a8d8066ef3ce5994be26
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-04-17 17:15:13 +0200

    SLI-913 Add copy button to the code snippets

---
+++ src/main/java/org/sonarlint/intellij/editor/SonarLintEditorWidgetActionProvider.kt
@@ -0,0 +1,35 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015-2023 SonarSource
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.editor
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorKind
+import com.intellij.openapi.editor.markup.InspectionWidgetActionProvider
+import org.sonarlint.intellij.editor.actions.CopyCodeExampleAction
+
+class SonarLintEditorWidgetActionProvider : InspectionWidgetActionProvider {
+    override fun createAction(editor: Editor): AnAction? {
+        if (editor.editorKind == EditorKind.UNTYPED) {
+            return CopyCodeExampleAction(editor)
+        }
+        return null
+    }
+}
