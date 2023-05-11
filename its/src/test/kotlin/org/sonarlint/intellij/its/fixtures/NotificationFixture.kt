374126444da2f725427ca645395e4c97bb6d2c60
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2023-05-11 14:05:15 +0200

    SLI-937 Highlight the security hotspots and add intention action

---
+++ its/src/test/kotlin/org/sonarlint/intellij/its/fixtures/NotificationFixture.kt
@@ -0,0 +1,54 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015-2023 SonarSource
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.its.fixtures
+
+import com.intellij.remoterobot.RemoteRobot
+import com.intellij.remoterobot.data.RemoteComponent
+import com.intellij.remoterobot.fixtures.CommonContainerFixture
+import com.intellij.remoterobot.fixtures.FixtureName
+import com.intellij.remoterobot.search.locators.byXpath
+import com.intellij.remoterobot.stepsProcessing.step
+import org.sonarlint.intellij.its.fixtures.tool.window.TabContentFixture
+import java.time.Duration
+
+fun RemoteRobot.notification(
+    title: String,
+    timeout: Duration = Duration.ofSeconds(20),
+    function: NotificationFixture.() -> Unit = {},
+): NotificationFixture = step("Search for dialog with title $title") {
+    val notification = find<NotificationFixture>(NotificationFixture.byTitle(title), timeout)
+
+    notification.apply(function)
+}
+
+@FixtureName("Notification")
+open class NotificationFixture(
+    remoteRobot: RemoteRobot,
+    remoteComponent: RemoteComponent,
+) : CommonContainerFixture(remoteRobot, remoteComponent) {
+
+    companion object {
+        fun byTitle(title: String) = byXpath("//div[@accessiblename='$title']")
+    }
+
+    fun content(classType: String, function: TabContentFixture.() -> Unit = {}) =
+        findElement<TabContentFixture>(byXpath("//div[@class='$classType']")).apply(function)
+
+}
