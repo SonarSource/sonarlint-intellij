0a2aec0a358618ffe524088a797d400d4c359fb0
Tobias Hahnen <tobias.hahnen@sonarsource.com>
2023-05-23 13:30:32 +0200

    SLI-833: Reworked the former solution
    Also added test class for later implementation.

---
+++ src/test/java/org/sonarlint/intellij/util/SonarLintAppUtilsTests.java
@@ -0,0 +1,7 @@
+package org.sonarlint.intellij.util;
+
+import org.sonarlint.intellij.AbstractSonarLintLightTests;
+
+public class SonarLintAppUtilsTests extends AbstractSonarLintLightTests {
+    /* NOT YET IMPLEMENTED */
+}
