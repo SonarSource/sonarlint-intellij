6db6b674b9e76c139f8b315a8449d40f638d320a
Eray Felek <eray.felek@sonarsource.com>
2023-06-19 17:16:27 +0200

    SLI-991 Update embedded JavaScript analyzer to 10.3.1.21905

--- build.gradle.kts
+++ build.gradle.kts
@@ -204,7 +204,7 @@ dependencies {
     testImplementation("org.eclipse.jetty:jetty-servlet:$jettyVersion")
     testImplementation("org.eclipse.jetty:jetty-proxy:$jettyVersion")
     "sqplugins"("org.sonarsource.java:sonar-java-plugin:7.20.0.31692")
-    "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:10.2.0.21568")
+    "sqplugins"("org.sonarsource.javascript:sonar-javascript-plugin:10.3.1.21905")
     "sqplugins"("org.sonarsource.php:sonar-php-plugin:3.30.0.9766")
     "sqplugins"("org.sonarsource.python:sonar-python-plugin:4.3.0.11660")
     "sqplugins"("org.sonarsource.kotlin:sonar-kotlin-plugin:2.15.0.2579")
