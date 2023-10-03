b901d0404eda8a52417faa2a706cac465f062ce9
Eray Felek <eray.felek@sonarsource.com>
2023-10-03 14:21:05 +0200

    SLI-1117 Update embedded Go analyzer to 1.15.0.4655

--- build.gradle.kts
+++ build.gradle.kts
@@ -213,7 +213,7 @@ dependencies {
     "sqplugins"("org.sonarsource.xml:sonar-xml-plugin:2.10.0.4108")
     "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.13.0.76953")
     "sqplugins"("org.sonarsource.text:sonar-text-plugin:2.4.0.2120")
-    "sqplugins"("org.sonarsource.slang:sonar-go-plugin:1.14.0.4481")
+    "sqplugins"("org.sonarsource.slang:sonar-go-plugin:1.15.0.4655")
     "sqplugins"("org.sonarsource.iac:sonar-iac-plugin:1.21.0.5999")
     if (artifactoryUsername.isNotEmpty() && artifactoryPassword.isNotEmpty()) {
         "sqplugins"("com.sonarsource.cpp:sonar-cfamily-plugin:6.49.0.62722")
