6d67484a1c11bff21bf239444bc392b469b37135
Eray Felek <eray.felek@sonarsource.com>
2023-10-13 16:47:34 +0200

    SLI-1132 Update embedded C#/VB.Net analyzer to 9.12.0.78982

--- build.gradle.kts
+++ build.gradle.kts
@@ -212,7 +212,7 @@ dependencies {
     "sqplugins"("org.sonarsource.slang:sonar-ruby-plugin:1.15.0.4655")
     "sqplugins"("org.sonarsource.html:sonar-html-plugin:3.9.0.3600")
     "sqplugins"("org.sonarsource.xml:sonar-xml-plugin:2.10.0.4108")
-    "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.13.0.76953")
+    "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.14.0.79083")
     "sqplugins"("org.sonarsource.text:sonar-text-plugin:2.4.0.2120")
     "sqplugins"("org.sonarsource.slang:sonar-go-plugin:1.15.0.4655")
     "sqplugins"("org.sonarsource.iac:sonar-iac-plugin:1.21.0.5999")
