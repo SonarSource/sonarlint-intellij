43085ea36de83f2bb50d116f2bc3e8ed7565f583
Eray Felek <eray.felek@sonarsource.com>
2023-10-18 13:18:05 +0200

    Update embedded SonarText analyzer to 2.5.0.2293

--- build.gradle.kts
+++ build.gradle.kts
@@ -228,7 +228,7 @@ dependencies {
     "sqplugins"("org.sonarsource.html:sonar-html-plugin:3.9.0.3600")
     "sqplugins"("org.sonarsource.xml:sonar-xml-plugin:2.10.0.4108")
     "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.14.0.79083")
-    "sqplugins"("org.sonarsource.text:sonar-text-plugin:2.4.0.2120")
+    "sqplugins"("org.sonarsource.text:sonar-text-plugin:2.5.0.2293")
     "sqplugins"("org.sonarsource.slang:sonar-go-plugin:1.15.0.4655")
     "sqplugins"("org.sonarsource.iac:sonar-iac-plugin:1.21.0.5999")
     if (artifactoryUsername.isNotEmpty() && artifactoryPassword.isNotEmpty()) {
