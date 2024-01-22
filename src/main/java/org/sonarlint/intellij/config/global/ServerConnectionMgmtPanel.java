faf368d51c07a9494a365ede6279596ed4fdb7ad
Eray Felek <eray.felek@sonarsource.com>
2024-01-22 13:14:57 +0100

    SLI-1226 Connection frame should be always visible

--- src/main/java/org/sonarlint/intellij/config/global/ServerConnectionMgmtPanel.java
+++ src/main/java/org/sonarlint/intellij/config/global/ServerConnectionMgmtPanel.java
@@ -135,7 +135,7 @@ public class ServerConnectionMgmtPanel implements ConfigurationPanel<SonarLintGl
     panel.add(optionsPanel);
     panel.add(serversPanel);
 
-    serversPanel.setMinimumSize(new Dimension(0, 225));
+    serversPanel.setPreferredSize(new Dimension(0, 125));
 
     connectionList.setCellRenderer(new ColoredListCellRenderer<>() {
       @Override
