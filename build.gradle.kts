82f724b3d80e6f1c42727391987555190d73528b
Eray Felek <eray.felek@sonarsource.com>
2024-01-25 12:49:00 +0100

    Update embedded C# analyzer to 9.17.0.82934

--- build.gradle.kts
+++ build.gradle.kts
@@ -221,7 +221,7 @@ dependencies {
     "sqplugins"("org.sonarsource.slang:sonar-ruby-plugin:1.15.0.4655")
     "sqplugins"("org.sonarsource.html:sonar-html-plugin:3.9.0.3600")
     "sqplugins"("org.sonarsource.xml:sonar-xml-plugin:2.10.0.4108")
-    "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.17.0.82356")
+    "sqplugins"("org.sonarsource.sonarlint.omnisharp:sonarlint-omnisharp-plugin:1.18.0.83276")
     "sqplugins"("org.sonarsource.text:sonar-text-plugin:2.7.1.1388")
     "sqplugins"("org.sonarsource.slang:sonar-go-plugin:1.15.0.4655")
     "sqplugins"("org.sonarsource.iac:sonar-iac-plugin:1.25.0.8192")
