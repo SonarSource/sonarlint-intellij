fa8a4ef72ba289a6377eb746bf274bb37d566ec0
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2024-05-07 16:24:18 +0200

    Fix missing package-info.java file

--- clion-common/build.gradle.kts
+++ clion-common/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     // Needed for https://github.com/gradle/gradle/issues/22333
     testRuntimeOnly(libs.junit.launcher)
     testRuntimeOnly(libs.junit.engine)
+    compileOnly(libs.findbugs.jsr305)
 }
 
 tasks.test {
--- clion-resharper/build.gradle.kts
+++ clion-resharper/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     // Needed for https://github.com/gradle/gradle/issues/22333
     testRuntimeOnly(libs.junit.launcher)
     testRuntimeOnly(libs.junit.engine)
+    compileOnly(libs.findbugs.jsr305)
 }
 
 tasks.test {
