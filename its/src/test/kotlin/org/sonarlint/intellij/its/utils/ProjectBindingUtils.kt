de29e4088d758a9a731e1dc94d69e835ecc2728b
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2024-07-03 14:28:21 +0200

    Revert "Test ellipsis windows"
    This reverts commit 629b8777d552021691fbce6366e26d062f28066f.

--- its/src/test/kotlin/org/sonarlint/intellij/its/utils/ProjectBindingUtils.kt
+++ its/src/test/kotlin/org/sonarlint/intellij/its/utils/ProjectBindingUtils.kt
@@ -94,7 +94,7 @@ class ProjectBindingUtils {
 
                 pressOk()
                 errorMessage("Project key for module 'sample-scala-module' should not be empty")
-                buttons(JButtonFixture.byText("Search in list"))[1].click()
+                buttons(JButtonFixture.byText("Search in list\u2026"))[1].click()
                 dialog("Select SonarQube Project To Bind") {
                     jList {
                         clickItem(MODULE_PROJECT_KEY, false)
