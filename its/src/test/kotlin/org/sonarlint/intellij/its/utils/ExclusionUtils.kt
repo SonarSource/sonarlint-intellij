3033729ab051cc365940812d5558f0eb5a256b80
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2024-07-15 13:26:49 +0200

    Fix exclude

--- its/src/test/kotlin/org/sonarlint/intellij/its/utils/ExclusionUtils.kt
+++ its/src/test/kotlin/org/sonarlint/intellij/its/utils/ExclusionUtils.kt
@@ -22,13 +22,13 @@ package org.sonarlint.intellij.its.utils
 import com.intellij.remoterobot.fixtures.ActionButtonFixture
 import com.intellij.remoterobot.utils.keyboard
 import com.intellij.remoterobot.utils.waitFor
+import java.time.Duration
 import org.sonarlint.intellij.its.BaseUiTest.Companion.remoteRobot
 import org.sonarlint.intellij.its.fixtures.clickWhenEnabled
 import org.sonarlint.intellij.its.fixtures.dialog
 import org.sonarlint.intellij.its.fixtures.idea
 import org.sonarlint.intellij.its.fixtures.jbTextFieldsWithBrowseButton
 import org.sonarlint.intellij.its.fixtures.tool.window.toolWindow
-import java.time.Duration
 
 class ExclusionUtils {
 
@@ -87,7 +87,7 @@ class ExclusionUtils {
                 idea {
                     dialog("Project Settings") {
                         findText("File Exclusions").click()
-                        actionButton(ActionButtonFixture.byTooltipText("Add")).clickWhenEnabled()
+                        actionButtons(ActionButtonFixture.byType())[0].clickWhenEnabled()
                         dialog("Add SonarLint File Exclusion") {
                             jbTextFieldsWithBrowseButton()[0].click()
                             keyboard { enterText(fileName) }
