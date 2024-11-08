6fd9f4a5c557e4e82d5b8b2ee4e351089422677e
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2024-11-08 10:14:56 +0100

    SLI-1694 Get rid of the Got It tooltips for new taxonomy

--- src/main/java/org/sonarlint/intellij/util/SonarGotItTooltipsUtils.kt
+++ src/main/java/org/sonarlint/intellij/util/SonarGotItTooltipsUtils.kt
@@ -23,43 +23,14 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.ui.GotItTooltip
 import java.awt.Point
-import java.net.URL
 import javax.swing.JComponent
 import org.sonarlint.intellij.SonarLintIcons
-import org.sonarlint.intellij.documentation.SonarLintDocumentation.Intellij.CLEAN_CODE_LINK
-import org.sonarlint.intellij.documentation.SonarLintDocumentation.Intellij.FOCUS_ON_NEW_CODE_LINK
 
 object SonarGotItTooltipsUtils {
-
-    private const val FOCUS_NEW_CODE_TOOLTIP_ID = "sonarlint.focus.new.code.tooltip"
-    private const val FOCUS_NEW_CODE_TOOLTIP_TEXT = """Deliver clean code by focusing on code that was recently modified"""
-
-    private const val CLEAN_CODE_TOOLTIP_ID = "sonarlint.clean.code.tooltip"
-    private const val CLEAN_CODE_TOOLTIP_TEXT = """We have refined Sonar issues: Clean Code attributes spotlight what characteristic of Clean Code was violated. 
-            Software qualities represent the impact of an issue on your application."""
 
     private const val TRAFFIC_LIGHT_TOOLTIP_ID = "sonarlint.traffic.light.tooltip"
     private const val TRAFFIC_LIGHT_TOOLTIP_TEXT = """See how many issues need your attention in the current file. 
         Click the icon to show/hide the SonarLint tool window, and hover to view more actions."""
-
-    fun showFocusOnNewCodeToolTip(component: JComponent, parent: Disposable) {
-        with(GotItTooltip(FOCUS_NEW_CODE_TOOLTIP_ID, FOCUS_NEW_CODE_TOOLTIP_TEXT, parent)) {
-            withIcon(SonarLintIcons.SONARLINT)
-            withPosition(Balloon.Position.atRight)
-            withBrowserLink("Learn more about Clean as You Code", URL(FOCUS_ON_NEW_CODE_LINK))
-            // Point at the first action level
-            show(component) { _, _ -> Point(0, 15) }
-        }
-    }
-
-    fun showCleanCodeToolTip(component: JComponent, parent: Disposable) {
-        with(GotItTooltip(CLEAN_CODE_TOOLTIP_ID, CLEAN_CODE_TOOLTIP_TEXT, parent)) {
-            withBrowserLink("Learn more about Clean Code", URL(CLEAN_CODE_LINK))
-            withIcon(SonarLintIcons.SONARLINT)
-            withPosition(Balloon.Position.atLeft)
-            show(component, GotItTooltip.LEFT_MIDDLE)
-        }
-    }
 
     fun showTrafficLightToolTip(component: JComponent, parent: Disposable) {
         // we pick a random project service that is disposable
