edbd9a914a574534348986f74da96bec5fc818ea
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2024-11-13 16:21:08 +0100

    SLI-1707 Add tests for backend service

--- src/test/java/org/sonarlint/intellij/core/BackendServiceTests.kt
+++ src/test/java/org/sonarlint/intellij/core/BackendServiceTests.kt
@@ -25,6 +25,7 @@ import com.intellij.openapi.project.ex.ProjectManagerEx
 import com.intellij.testFramework.replaceService
 import java.nio.file.Path
 import java.time.Duration
+import java.util.UUID
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
 import org.assertj.core.api.Assertions.assertThat
@@ -44,17 +45,37 @@ import org.sonarlint.intellij.AbstractSonarLintHeavyTests
 import org.sonarlint.intellij.config.global.ServerConnection
 import org.sonarlint.intellij.config.global.SonarLintGlobalSettings
 import org.sonarlint.intellij.messages.GlobalConfigurationListener
+import org.sonarsource.sonarlint.core.client.utils.IssueResolutionStatus
 import org.sonarsource.sonarlint.core.rpc.client.Sloop
 import org.sonarsource.sonarlint.core.rpc.client.SloopLauncher
 import org.sonarsource.sonarlint.core.rpc.protocol.SonarLintRpcServer
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.binding.BindingRpcService
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.binding.GetSharedConnectedModeConfigFileParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.branch.DidVcsRepositoryChangeParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.branch.SonarProjectBranchRpcService
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.config.ConfigurationRpcService
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.config.binding.DidUpdateBindingParams
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.config.scope.DidAddConfigurationScopesParams
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.config.scope.DidRemoveConfigurationScopeParams
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.connection.ConnectionRpcService
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.connection.check.CheckSmartNotificationsSupportedParams
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.connection.config.DidChangeCredentialsParams
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.connection.config.DidUpdateConnectionsParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.connection.org.GetOrganizationParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.connection.org.ListUserOrganizationsParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.hotspot.ChangeHotspotStatusParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.hotspot.CheckLocalDetectionSupportedParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.hotspot.CheckStatusChangePermittedParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.hotspot.HotspotRpcService
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.hotspot.HotspotStatus
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.hotspot.OpenHotspotInBrowserParams
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.InitializeParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.issue.AddIssueCommentParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.issue.ChangeIssueStatusParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.issue.GetEffectiveIssueDetailsParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.issue.IssueRpcService
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.issue.ReopenIssueParams
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.issue.ResolutionStatus
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.tracking.ListAllResponse
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.tracking.TaintVulnerabilityTrackingRpcService
 
@@ -64,6 +85,10 @@ class BackendServiceTests : AbstractSonarLintHeavyTests() {
     private lateinit var backend: SonarLintRpcServer
     private lateinit var backendConnectionService: ConnectionRpcService
     private lateinit var backendConfigurationService: ConfigurationRpcService
+    private lateinit var backendIssueService: IssueRpcService
+    private lateinit var backendBindingService: BindingRpcService
+    private lateinit var backendHotspotService: HotspotRpcService
+    private lateinit var backendBranchService: SonarProjectBranchRpcService
     private lateinit var service: BackendService
 
     override fun initApplication() {
@@ -79,10 +104,18 @@ class BackendServiceTests : AbstractSonarLintHeavyTests() {
         `when`(backend.initialize(any())).thenReturn(CompletableFuture.completedFuture(null))
         backendConnectionService = mock(ConnectionRpcService::class.java)
         backendConfigurationService = mock(ConfigurationRpcService::class.java)
+        backendIssueService = mock(IssueRpcService::class.java)
+        backendBindingService = mock(BindingRpcService::class.java)
+        backendHotspotService = mock(HotspotRpcService::class.java)
+        backendBranchService = mock(SonarProjectBranchRpcService::class.java)
         val taintService = mock(TaintVulnerabilityTrackingRpcService::class.java)
         `when`(taintService.listAll(any())).thenReturn(CompletableFuture.completedFuture(ListAllResponse(emptyList())))
         `when`(backend.connectionService).thenReturn(backendConnectionService)
         `when`(backend.configurationService).thenReturn(backendConfigurationService)
+        `when`(backend.issueService).thenReturn(backendIssueService)
+        `when`(backend.bindingService).thenReturn(backendBindingService)
+        `when`(backend.hotspotService).thenReturn(backendHotspotService)
+        `when`(backend.sonarProjectBranchService).thenReturn(backendBranchService)
         `when`(backend.taintVulnerabilityTrackingService).thenReturn(taintService)
         sloop = mock(Sloop::class.java)
         `when`(sloop.rpcServer).thenReturn(backend)
@@ -363,4 +396,218 @@ class BackendServiceTests : AbstractSonarLintHeavyTests() {
 
         verify(backend, timeout(500)).shutdown()
     }
+
+    @Test
+    fun test_get_issue_details() {
+        val issueId = UUID.randomUUID()
+
+        service.getIssueDetails(module, issueId)
+
+        val paramsCaptor = argumentCaptor<GetEffectiveIssueDetailsParams>()
+        verify(backendIssueService, timeout(500)).getEffectiveIssueDetails(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "configurationScopeId",
+            "issueId"
+        ).containsExactly(moduleBackendId(module), issueId)
+    }
+
+    @Test
+    fun test_get_shared_connected_mode_config() {
+        service.getSharedConnectedModeConfigFileContents(project)
+
+        val paramsCaptor = argumentCaptor<GetSharedConnectedModeConfigFileParams>()
+        verify(backendBindingService, timeout(500)).getSharedConnectedModeConfigFileContents(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.configScopeId).isEqualTo(projectBackendId(project))
+    }
+
+    @Test
+    fun test_open_hotspot_in_browser() {
+        val hotspotKey = "key"
+
+        service.openHotspotInBrowser(module, hotspotKey)
+
+        val paramsCaptor = argumentCaptor<OpenHotspotInBrowserParams>()
+        verify(backendHotspotService, timeout(500)).openHotspotInBrowser(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "configScopeId",
+            "hotspotKey"
+        ).containsExactly(moduleBackendId(module), hotspotKey)
+    }
+
+    @Test
+    fun test_check_local_detection_for_hotspot() {
+        service.checkLocalSecurityHotspotDetectionSupported(project)
+
+        val paramsCaptor = argumentCaptor<CheckLocalDetectionSupportedParams>()
+        verify(backendHotspotService, timeout(500)).checkLocalDetectionSupported(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.configScopeId).isEqualTo(projectBackendId(project))
+    }
+
+    @Test
+    fun test_change_status_for_hotspot() {
+        val hotspotKey = "key"
+        val newStatus = HotspotStatus.FIXED
+
+        service.changeStatusForHotspot(module, hotspotKey, newStatus)
+
+        val paramsCaptor = argumentCaptor<ChangeHotspotStatusParams>()
+        verify(backendHotspotService, timeout(500)).changeStatus(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "configurationScopeId",
+            "hotspotKey",
+            "newStatus"
+        ).containsExactly(moduleBackendId(module), hotspotKey, newStatus)
+    }
+
+    @Test
+    fun test_mark_as_resolved() {
+        val issueKey = "key"
+        val newStatus = IssueResolutionStatus.ACCEPT
+        val isTaint = false
+
+        service.markAsResolved(module, issueKey, newStatus, isTaint)
+
+        val paramsCaptor = argumentCaptor<ChangeIssueStatusParams>()
+        verify(backendIssueService, timeout(500)).changeStatus(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "configurationScopeId",
+            "issueKey",
+            "newStatus",
+            "isTaintIssue"
+        ).containsExactly(moduleBackendId(module), issueKey, ResolutionStatus.valueOf(newStatus.name), isTaint)
+    }
+
+    @Test
+    fun test_reopen_issue() {
+        val issueId = "id"
+        val isTaint = false
+
+        service.reopenIssue(module, issueId, isTaint)
+
+        val paramsCaptor = argumentCaptor<ReopenIssueParams>()
+        verify(backendIssueService, timeout(500)).reopenIssue(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "configurationScopeId",
+            "issueId",
+            "isTaintIssue"
+        ).containsExactly(moduleBackendId(module), issueId, isTaint)
+    }
+
+    @Test
+    fun test_comment_issue() {
+        val issueKey = "key"
+        val comment = "comment"
+
+        service.addCommentOnIssue(module, issueKey, comment)
+
+        val paramsCaptor = argumentCaptor<AddIssueCommentParams>()
+        verify(backendIssueService, timeout(500)).addComment(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "configurationScopeId",
+            "issueKey",
+            "text"
+        ).containsExactly(moduleBackendId(module), issueKey, comment)
+    }
+
+    @Test
+    fun test_hotspot_change_permitted() {
+        val connectionId = "id"
+        val hotspotKey = "key"
+
+        service.checkStatusChangePermitted(connectionId, hotspotKey)
+
+        val paramsCaptor = argumentCaptor<CheckStatusChangePermittedParams>()
+        verify(backendHotspotService, timeout(500)).checkStatusChangePermitted(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "connectionId",
+            "hotspotKey"
+        ).containsExactly(connectionId, hotspotKey)
+    }
+
+    @Test
+    fun test_issue_change_permitted() {
+        val connectionId = "id"
+        val issueKey = "key"
+
+        service.checkIssueStatusChangePermitted(connectionId, issueKey)
+
+        val paramsCaptor = argumentCaptor<org.sonarsource.sonarlint.core.rpc.protocol.backend.issue.CheckStatusChangePermittedParams>()
+        verify(backendIssueService, timeout(500)).checkStatusChangePermitted(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue).extracting(
+            "connectionId",
+            "issueKey"
+        ).containsExactly(connectionId, issueKey)
+    }
+
+    @Test
+    fun test_did_vcs_repo_change() {
+        service.didVcsRepoChange(project)
+
+        val paramsCaptor = argumentCaptor<DidVcsRepositoryChangeParams>()
+        verify(backendBranchService, timeout(500)).didVcsRepositoryChange(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.configurationScopeId).isEqualTo(projectBackendId(project))
+    }
+
+    @Test
+    fun test_check_sonarqube_smart_notification_supported() {
+        val serverConnection = ServerConnection.newBuilder().setName("id").setHostUrl("url").build()
+
+        service.checkSmartNotificationsSupported(serverConnection)
+
+        val paramsCaptor = argumentCaptor<CheckSmartNotificationsSupportedParams>()
+        verify(backendConnectionService, timeout(500)).checkSmartNotificationsSupported(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.transientConnection.isLeft).isTrue()
+        assertThat(paramsCaptor.firstValue.transientConnection.left.serverUrl).isEqualTo("url")
+    }
+
+    @Test
+    fun test_check_sonarcloud_smart_notification_supported() {
+        val serverConnection =
+            ServerConnection.newBuilder().setName("id").setHostUrl("https://sonarcloud.io").setOrganizationKey("org").build()
+
+        service.checkSmartNotificationsSupported(serverConnection)
+
+        val paramsCaptor = argumentCaptor<CheckSmartNotificationsSupportedParams>()
+        verify(backendConnectionService, timeout(500)).checkSmartNotificationsSupported(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.transientConnection.isRight).isTrue()
+        assertThat(paramsCaptor.firstValue.transientConnection.right.organization).isEqualTo("org")
+    }
+
+    @Test
+    fun test_check_list_user_organizations_for_sonarqube() {
+        val serverConnection = ServerConnection.newBuilder().setName("id").setHostUrl("url").build()
+
+        service.listUserOrganizations(serverConnection)
+
+        val paramsCaptor = argumentCaptor<ListUserOrganizationsParams>()
+        verify(backendConnectionService, timeout(500)).listUserOrganizations(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.credentials.isRight).isTrue()
+    }
+
+    @Test
+    fun test_check_list_user_organizations_for_sonarcloud() {
+        val serverConnection =
+            ServerConnection.newBuilder().setName("id").setHostUrl("https://sonarcloud.io").setOrganizationKey("org").build()
+
+        service.listUserOrganizations(serverConnection)
+
+        val paramsCaptor = argumentCaptor<ListUserOrganizationsParams>()
+        verify(backendConnectionService, timeout(500)).listUserOrganizations(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.credentials.isRight).isTrue()
+    }
+
+    @Test
+    fun test_get_organization() {
+        val serverConnection =
+            ServerConnection.newBuilder().setName("id").setHostUrl("https://sonarcloud.io").setOrganizationKey("org").build()
+        val organizationKey = "org"
+
+        service.getOrganization(serverConnection, organizationKey)
+
+        val paramsCaptor = argumentCaptor<GetOrganizationParams>()
+        verify(backendConnectionService, timeout(500)).getOrganization(paramsCaptor.capture())
+        assertThat(paramsCaptor.firstValue.organizationKey).isEqualTo(organizationKey)
+        assertThat(paramsCaptor.firstValue.credentials.isRight).isTrue()
+    }
+
 }
