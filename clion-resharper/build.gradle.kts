3ad6d1f52648a476908cff03251f87d42e4835f1
Eray Felek <eray.felek@sonarsource.com>
2024-11-18 15:56:03 +0100

    SLI-1710 Use custom AMI Windows Image
    workaround for Packages folder with microsoft-openjdk

--- clion-resharper/build.gradle.kts
+++ clion-resharper/build.gradle.kts
@@ -3,9 +3,11 @@ val resharperHome: String? = System.getenv("RESHARPER_HOME")
 
 intellij {
     if (!resharperHome.isNullOrBlank()) {
+        println("Using local installation of IntelliJ IDEA: $resharperHome")
         localPath.set(resharperHome)
         localSourcesPath.set(resharperHome)
     } else {
+        println("No local installation of IntelliJ IDEA found, using version $clionResharperBuildVersion")
         version.set(clionResharperBuildVersion)
     }
 }
