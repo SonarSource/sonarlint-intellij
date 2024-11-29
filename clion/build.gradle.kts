abbf6a18d901be1480ad8ef973d04c17dc554e4f
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2024-11-29 14:53:29 +0100

    Remove redundant dependencies

--- clion/build.gradle.kts
+++ clion/build.gradle.kts
@@ -18,8 +18,6 @@ dependencies {
     implementation(project(":clion-common"))
     testImplementation(libs.junit.api)
     testImplementation(libs.mockito.core)
-    testImplementation(libs.junit.api)
-    testRuntimeOnly(libs.junit.engine)
     testRuntimeOnly(libs.junit.engine)
 }
 
