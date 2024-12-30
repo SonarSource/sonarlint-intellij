40e1319cc484766aa38b72a677abb9082b4efdaa
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2024-12-30 11:22:06 +0100

    Run the plugin verifier on 2024.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -157,7 +157,7 @@ tasks.runPluginVerifier {
         ideVersions.set(verifierVersions.split(','))
     } else {
         // Test oldest supported, and latest
-        ideVersions.set(listOf("IC-2022.3.1", "IC-2024.1.4"))
+        ideVersions.set(listOf("IC-2022.3.1", "IC-2024.3.1"))
     }
     failureLevel.set(
         EnumSet.complementOf(
