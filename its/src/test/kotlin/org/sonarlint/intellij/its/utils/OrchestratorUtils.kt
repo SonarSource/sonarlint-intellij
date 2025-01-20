868c9616214d45ae372cb75adf3892707f26df64
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2025-01-20 16:31:24 +0100

    SLI-1808 Remove `sonar.password` from SonarQube Server ITs

--- its/src/test/kotlin/org/sonarlint/intellij/its/utils/OrchestratorUtils.kt
+++ its/src/test/kotlin/org/sonarlint/intellij/its/utils/OrchestratorUtils.kt
@@ -24,13 +24,13 @@ import com.sonar.orchestrator.build.SonarScanner
 import com.sonar.orchestrator.container.Server
 import com.sonar.orchestrator.junit5.OrchestratorExtension
 import com.sonar.orchestrator.junit5.OrchestratorExtensionBuilder
+import java.io.File
 import org.sonarlint.intellij.its.utils.ItUtils.SONAR_VERSION
 import org.sonarqube.ws.client.HttpConnector
 import org.sonarqube.ws.client.WsClient
 import org.sonarqube.ws.client.WsClientFactories
 import org.sonarqube.ws.client.users.CreateRequest
 import org.sonarqube.ws.client.usertokens.GenerateRequest
-import java.io.File
 
 class OrchestratorUtils {
 
@@ -59,21 +59,31 @@ class OrchestratorUtils {
         }
 
         fun executeBuildWithMaven(filePath: String, orchestrator: OrchestratorExtension) {
-            orchestrator.executeBuild(
-                MavenBuild.create(File(filePath))
-                    .setCleanPackageSonarGoals()
-                    .setProperty("sonar.login", SONARLINT_USER)
-                    .setProperty("sonar.password", SONARLINT_PWD)
-            )
+            var mavenBuild = MavenBuild.create(File(filePath))
+                .setCleanPackageSonarGoals()
+
+            mavenBuild = if (orchestrator.server.version().isGreaterThanOrEquals(10, 2)) {
+                mavenBuild.setProperty("sonar.token", orchestrator.defaultAdminToken)
+            } else {
+                mavenBuild.setProperty("sonar.login", Server.ADMIN_LOGIN)
+                    .setProperty("sonar.password", Server.ADMIN_PASSWORD)
+            }
+
+            orchestrator.executeBuild(mavenBuild)
         }
 
         fun executeBuildWithSonarScanner(filePath: String, orchestrator: OrchestratorExtension, projectKey: String) {
-            orchestrator.executeBuild(
-                SonarScanner.create(File(filePath))
-                    .setProperty("sonar.login", SONARLINT_USER)
-                    .setProperty("sonar.password", SONARLINT_PWD)
+            var scanner = SonarScanner.create(File(filePath))
                     .setProperty("sonar.projectKey", projectKey)
-            )
+
+            scanner = if (orchestrator.server.version().isGreaterThanOrEquals(10, 2)) {
+                scanner.setProperty("sonar.token", orchestrator.defaultAdminToken)
+            } else {
+                scanner.setProperty("sonar.login", Server.ADMIN_LOGIN)
+                    .setProperty("sonar.password", Server.ADMIN_PASSWORD)
+            }
+
+            orchestrator.executeBuild(scanner)
         }
 
         fun generateTokenNameAndValue(wsClient: WsClient, userName: String): Pair<String, String> {
