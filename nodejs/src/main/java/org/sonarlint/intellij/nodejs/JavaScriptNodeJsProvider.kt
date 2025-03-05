f842e3dcc9f731394fe2fc49903e300c322f77f0
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-03-05 10:26:37 +0100

    Improvements

--- nodejs/src/main/java/org/sonarlint/intellij/nodejs/JavaScriptNodeJsProvider.kt
+++ nodejs/src/main/java/org/sonarlint/intellij/nodejs/JavaScriptNodeJsProvider.kt
@@ -19,20 +19,13 @@
  */
 package org.sonarlint.intellij.nodejs;
 
-import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager;
-import com.intellij.openapi.project.Project;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import org.jetbrains.annotations.Nullable;
-import org.sonarlint.intellij.common.util.NodeJsProvider;
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
+import com.intellij.openapi.project.Project
+import java.nio.file.Paths
+import org.sonarlint.intellij.common.util.NodeJsProvider
 
-public class JavaScriptNodeJsProvider implements NodeJsProvider {
+class JavaScriptNodeJsProvider : NodeJsProvider {
 
-  @Nullable
-  @Override
-  public Path getNodeJsPathFor(Project project) {
-    var interpreter = NodeJsInterpreterManager.getInstance(project).getInterpreter();
-    return interpreter != null ? Paths.get(interpreter.getReferenceName()) : null;
-  }
+  override fun getNodeJsPathFor(project: Project) = NodeJsInterpreterManager.getInstance(project).interpreter?.let { Paths.get(it.referenceName) }
 
 }
