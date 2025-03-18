645a97f562ff0f5da508b083b54e0b094769f375
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-03-18 11:05:18 +0100

    SLI-1531 Update to IntelliJ Platform Gradle Plugin (2.x)

--- git/build.gradle.kts
+++ git/build.gradle.kts
@@ -1,20 +1,22 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
 val intellijBuildVersion: String by project
 val ideaHome: String? = System.getenv("IDEA_HOME")
 
 plugins {
+    id("org.jetbrains.intellij.platform.module")
     kotlin("jvm")
 }
 
-intellij {
-    if (!ideaHome.isNullOrBlank()) {
-        localPath.set(ideaHome)
-        localSourcesPath.set(ideaHome)
-    } else {
-        version.set(intellijBuildVersion)
-    }
-    plugins.set(listOf("Git4Idea"))
-}
-
 dependencies {
+    intellijPlatform {
+        if (!ideaHome.isNullOrBlank()) {
+            local(ideaHome)
+        } else {
+            intellijIdeaCommunity(intellijBuildVersion)
+        }
+        bundledPlugins("Git4Idea")
+        testFramework(TestFrameworkType.Platform)
+    }
     implementation(project(":common"))
 }
