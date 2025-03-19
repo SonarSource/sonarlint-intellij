62a6ade556ab0c4797c841419e364380fb4ad6a8
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-03-19 12:18:37 +0100

    Debug its

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
 rootProject.name = "sonarlint-intellij"
 include("its", "clion", "clion-resharper", "nodejs", "clion-common", "common", "git", "rider")
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
+}
+
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
@@ -25,8 +29,4 @@ buildCache {
         isEnabled = isCiServer
         isPush = isMasterBranch
     }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
 }
