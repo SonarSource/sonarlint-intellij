91e8237ec834daaf262615426dc29e48d0cdca91
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-03-19 12:18:37 +0100

    Debug its

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,7 @@ rootProject.name = "sonarlint-intellij"
 include("its", "clion", "clion-resharper", "nodejs", "clion-common", "common", "git", "rider")
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-    id("com.gradle.develocity") version "3.18.2"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.2.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
 }
 
 dependencyResolutionManagement {
