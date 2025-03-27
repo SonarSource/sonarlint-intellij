a31d3ad45205648630d2fcec1a1482771cb50e2f
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-03-27 09:52:14 +0100

    FrameworkTypeTest fix

--- clion-common/build.gradle.kts
+++ clion-common/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
 val clionResharperBuildVersion: String by project
 val resharperHome: String? = System.getenv("RESHARPER_HOME")
 
@@ -15,6 +17,7 @@ dependencies {
             println("No local installation of CLion found, using version $clionResharperBuildVersion")
             rider(clionResharperBuildVersion, useInstaller = false)
         }
+        testFramework(TestFrameworkType.Platform)
     }
     implementation(project(":common"))
     testImplementation(libs.junit.api)
