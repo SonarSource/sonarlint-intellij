59fa87c834fbebdaba4625d6518510877ea9cd45
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-03-27 15:00:41 +0100

    Fix dispose

--- src/main/java/org/sonarlint/intellij/core/BackendService.kt
+++ src/main/java/org/sonarlint/intellij/core/BackendService.kt
@@ -568,7 +568,9 @@ class BackendService : Disposable {
     }
 
     internal fun projectClosed(project: Project) {
-        ModuleManager.getInstance(project).modules.forEach { moduleRemoved(it) }
+        ModuleManager.getInstance(project).modules.forEach {
+            if (!it.isDisposed) { moduleRemoved(it) }
+        }
         val projectId = projectId(project)
         notifyBackend { it.configurationService.didRemoveConfigurationScope(DidRemoveConfigurationScopeParams(projectId)) }
     }
