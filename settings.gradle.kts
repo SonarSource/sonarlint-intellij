641dc3ffc754968f9ca6e9087cfe280029c0daf0
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-04-08 14:40:25 +0200

    Develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ include("its", "clion", "clion-resharper", "nodejs", "clion-common", "common", "
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
+    id("com.gradle.develocity") version("3.18.2")
 }
 
 dependencyResolutionManagement {
