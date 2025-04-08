f88a08ad0abb8b788c8956d73a19e8d42d6dd60b
Eray Felek <eray.felek@sonarsource.com>
2025-04-08 16:04:57 +0200

    Add test for walkthrough

---
+++ its/src/test/kotlin/org/sonarlint/intellij/its/tests/domain/WalkthroughTests.kt
@@ -0,0 +1,60 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015-2025 SonarSource
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.its.tests.domain
+
+import com.intellij.remoterobot.utils.WaitForConditionTimeoutException
+import com.intellij.remoterobot.utils.keyboard
+import org.junit.jupiter.api.fail
+import org.sonarlint.intellij.its.BaseUiTest.Companion.remoteRobot
+import org.sonarlint.intellij.its.fixtures.idea
+import org.sonarlint.intellij.its.fixtures.tool.window.toolWindow
+import org.sonarlint.intellij.its.fixtures.walkthroughComponent
+import java.awt.event.KeyEvent
+
+class WalkthroughTests {
+
+    companion object {
+        fun closeWalkthrough() {
+            with(remoteRobot) {
+                idea {
+                    toolWindow("Welcome to SonarQube for IDE") {
+                        findText("Next: Learn as You Code").click()
+                        keyboard { hotKey(KeyEvent.VK_SHIFT, KeyEvent.VK_ESCAPE) }
+                    }
+                }
+            }
+        }
+
+        fun verifyWalkthroughIsNotShowing() {
+            with(remoteRobot) {
+                idea {
+                    try {
+                        if (walkthroughComponent().isShowing) {
+                            fail("Walkthrough is showing")
+                        }
+                    } catch (e: WaitForConditionTimeoutException) {
+                        // If the walkthrough is not showing, it should pass
+                    }
+                }
+            }
+        }
+    }
+
+}
