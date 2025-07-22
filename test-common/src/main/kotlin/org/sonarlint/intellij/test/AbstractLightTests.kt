4086ded16e0e868f88ee7caadd4f95acdd75fe42
Viacheslav Tsuvariev <viacheslav.tsuvariev@sonarsource.com>
2025-07-22 16:46:54 +0200

    SLI-2136 Create a new module for common test logic

---
+++ test-common/src/main/kotlin/org/sonarlint/intellij/test/AbstractLightTests.kt
@@ -0,0 +1,43 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015-2025 SonarSource
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.test
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import java.lang.reflect.Method
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInfo
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(RunInEdtInterceptor::class)
+abstract class AbstractLightTests : BasePlatformTestCase() {
+
+  @BeforeEach
+  fun beforeEachLightTest(testInfo: TestInfo) {
+    // explicitly call TestCase.setName as IntelliJ relies on it for the setup
+    name = testInfo.testMethod.map(Method::getName).orElseGet { "test" }
+    super.setUp()
+  }
+
+  @AfterEach
+  fun afterEachLightTest() {
+    super.tearDown()
+  }
+}
