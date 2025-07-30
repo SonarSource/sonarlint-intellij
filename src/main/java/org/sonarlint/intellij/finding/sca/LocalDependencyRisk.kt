a979a71625a1467ee0b5af3e6443ce4a581e9492
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2025-07-30 16:01:20 +0200

    SLI-2169 Implement caching of Dependency Risks on IntelliJ side

---
+++ src/main/java/org/sonarlint/intellij/finding/sca/LocalDependencyRisk.kt
@@ -0,0 +1,30 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015-2025 SonarSource
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.finding.sca
+
+import java.util.UUID
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.tracking.DependencyRiskDto
+
+class LocalDependencyRisk(serverDependencyRisk: DependencyRiskDto) {
+
+    val id: UUID = serverDependencyRisk.id
+    val isResolved = serverDependencyRisk.status in listOf(DependencyRiskDto.Status.SAFE, DependencyRiskDto.Status.ACCEPT)
+
+}
