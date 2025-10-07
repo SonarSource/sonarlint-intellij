9cb96e4c43e024d66e1c8441659bcc6145310660
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2025-10-07 21:11:28 +0200

    Push capability

--- src/main/java/org/sonarlint/intellij/core/BackendService.kt
+++ src/main/java/org/sonarlint/intellij/core/BackendService.kt
@@ -403,7 +403,8 @@ class BackendService : Disposable {
             BackendCapability.PROJECT_SYNCHRONIZATION,
             BackendCapability.SMART_NOTIFICATIONS,
             BackendCapability.ISSUE_STREAMING,
-            BackendCapability.SCA_SYNCHRONIZATION
+            BackendCapability.SCA_SYNCHRONIZATION,
+            BackendCapability.CONTEXT_INDEXING_ENABLED
         )
         if (!System.getProperty("sonarlint.telemetry.disabled", "false").toBoolean()) {
             capabilities.add(BackendCapability.TELEMETRY)
