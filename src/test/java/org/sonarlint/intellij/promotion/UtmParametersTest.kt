9f2cd2c19abda2863832b6acfbc6ca45561d8c48
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2025-11-14 09:13:17 +0100

    SLI-2333 Update code file license headers to "SonarSource Sàrl" (#1569)

--- src/test/java/org/sonarlint/intellij/promotion/UtmParametersTest.kt
+++ src/test/java/org/sonarlint/intellij/promotion/UtmParametersTest.kt
@@ -1,6 +1,6 @@
 /*
  * SonarLint for IntelliJ IDEA
- * Copyright (C) 2015-2025 SonarSource
+ * Copyright (C) 2015-2025 SonarSource Sàrl
  * sonarlint@sonarsource.com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,7 +17,6 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
-
 package org.sonarlint.intellij.promotion
 
 import org.assertj.core.api.Assertions.assertThat
