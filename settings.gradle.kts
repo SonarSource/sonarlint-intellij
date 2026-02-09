a8b3132131d04832775cb1889b3672c1be49e60c
Damien Urruty <damien.urruty@sonarsource.com>
2026-02-09 18:07:31 +0100

    SLI-2484 Bump intellij-platform-gradle plugin to latest version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
 plugins {
-    id("org.jetbrains.intellij.platform.settings") version "2.10.5"
+    id("org.jetbrains.intellij.platform.settings") version "2.11.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     id("com.gradle.develocity") version "4.0"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.2.1"
