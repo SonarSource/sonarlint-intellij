9fe3247ea0a0c1a20eee8b54ab5976c152ebf4d9
Viacheslav Tsuvariev <viacheslav.tsuvariev@sonarsource.com>
2026-03-13 15:52:36 +0100

    SLI-2473 Move renderers to a separate classes and minor fixes.

---
+++ src/main/java/org/sonarlint/intellij/config/project/LanguageCellRenderer.kt
@@ -0,0 +1,46 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015-2025 SonarSource Sàrl
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.config.project
+
+import com.intellij.openapi.fileTypes.UnknownFileType
+import com.intellij.util.ui.EmptyIcon
+import com.intellij.util.ui.JBUI
+import java.awt.Component
+import javax.swing.JLabel
+import javax.swing.JTable
+import javax.swing.table.DefaultTableCellRenderer
+import org.sonarlint.intellij.ui.ruledescription.RuleLanguages
+
+private val CELL_PADDING = JBUI.Borders.empty(0, 8)
+
+class LanguageCellRenderer : DefaultTableCellRenderer() {
+    override fun getTableCellRendererComponent(
+        table: JTable, value: Any?, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int,
+    ): Component {
+        val label = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column) as JLabel
+        if (value is SupportedLanguageRow) {
+            val fileType = RuleLanguages.findFileTypeByRuleLanguage(value.language)
+            label.icon = if (fileType is UnknownFileType) EmptyIcon.ICON_16 else fileType.icon
+            label.text = value.displayName
+        }
+        label.border = CELL_PADDING
+        return label
+    }
+}
---
+++ src/main/java/org/sonarlint/intellij/config/project/SourceCellRenderer.kt
@@ -0,0 +1,60 @@
+/*
+ * SonarLint for IntelliJ IDEA
+ * Copyright (C) 2015-2025 SonarSource Sàrl
+ * sonarlint@sonarsource.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonarlint.intellij.config.project
+
+import com.intellij.ui.JBColor
+import com.intellij.util.ui.EmptyIcon
+import com.intellij.util.ui.JBUI
+import java.awt.Color
+import java.awt.Component
+import javax.swing.JLabel
+import javax.swing.JTable
+import javax.swing.table.DefaultTableCellRenderer
+import org.sonarlint.intellij.SonarLintPlugin
+import org.sonarlint.intellij.common.util.SonarLintUtils
+import org.sonarlint.intellij.ui.icons.SonarLintIcons
+
+private val COLOR_BLUE = JBColor(Color(30, 100, 200), Color(100, 160, 255))
+private val CELL_PADDING = JBUI.Borders.empty(0, 8)
+
+class SourceCellRenderer : DefaultTableCellRenderer() {
+    private val pluginVersion: String = SonarLintUtils.getService(SonarLintPlugin::class.java).version
+
+    override fun getTableCellRendererComponent(table: JTable, value: Any?, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component {
+        val label = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column) as JLabel
+        if (value is AnalyzerSource) {
+            label.icon = when (value) {
+                AnalyzerSource.SONARQUBE_SERVER -> SonarLintIcons.ICON_SONARQUBE_SERVER_16
+                AnalyzerSource.SONARQUBE_CLOUD -> SonarLintIcons.ICON_SONARQUBE_CLOUD_16
+                AnalyzerSource.LOCAL -> EmptyIcon.ICON_16
+            }
+            label.text = when (value) {
+                AnalyzerSource.LOCAL -> "${value.label} $pluginVersion"
+                else -> value.label
+            }
+            if (!isSelected && value != AnalyzerSource.LOCAL) {
+                label.foreground = COLOR_BLUE
+            }
+        }
+        label.horizontalAlignment = LEFT
+        label.border = CELL_PADDING
+        return label
+    }
+}
