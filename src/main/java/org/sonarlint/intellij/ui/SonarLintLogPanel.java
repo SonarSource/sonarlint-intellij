da1a352fbfa9dfdc28be7341de4fad440ce7e937
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2026-04-01 19:51:49 +0200

    SLI-2487 Fix memory leaks in the log panel

--- src/main/java/org/sonarlint/intellij/ui/SonarLintLogPanel.java
+++ src/main/java/org/sonarlint/intellij/ui/SonarLintLogPanel.java
@@ -22,38 +22,31 @@ package org.sonarlint.intellij.ui;
 import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
-import com.intellij.openapi.wm.ToolWindow;
 import javax.swing.Box;
-import org.sonarlint.intellij.actions.ToolWindowVerboseModeAction;
 import org.sonarlint.intellij.common.ui.SonarLintConsole;
 import org.sonarlint.intellij.common.util.SonarLintUtils;
 import org.sonarlint.intellij.util.SonarLintActions;
 
 public class SonarLintLogPanel extends SimpleToolWindowPanel {
+
   private static final String ID = "SonarQube for IDE";
 
-  private final ToolWindow toolWindow;
   private final Project project;
 
-  private ActionToolbar mainToolbar;
-
-  public SonarLintLogPanel(ToolWindow toolWindow, Project project) {
+  public SonarLintLogPanel(Project project) {
     super(false, false);
-    this.toolWindow = toolWindow;
     this.project = project;
 
-    addLogActions();
     addToolbar();
     addConsole();
   }
 
   private void addToolbar() {
     var actionGroup = createActionGroup();
-    mainToolbar = ActionManager.getInstance().createActionToolbar(ID, actionGroup, false);
+    var mainToolbar = ActionManager.getInstance().createActionToolbar(ID, actionGroup, false);
     mainToolbar.setTargetComponent(this);
     var toolBarBox = Box.createHorizontalBox();
     toolBarBox.add(mainToolbar.getComponent());
@@ -70,15 +63,10 @@ public class SonarLintLogPanel extends SimpleToolWindowPanel {
     return actionGroup;
   }
 
-  private void addLogActions() {
-    var group = new DefaultActionGroup();
-    group.add(new ToolWindowVerboseModeAction());
-    toolWindow.setAdditionalGearActions(group);
-  }
-
   private void addConsole() {
     var consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
     SonarLintUtils.getService(project, SonarLintConsole.class).setConsoleView(consoleView);
     super.setContent(consoleView.getComponent());
   }
+
 }
